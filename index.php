f08931b31336974038afdc9803c992b78ae162c9
zhaoyu <zhaoyuhui@csair.com>
2018-08-10 17:38:07 +0800

    frame build, temp commit

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
-// date
-echo 'aaa'
 
+if (!defined('WIKIBOX_APP_NAME')) {
+    define('WIKIBOX_APP_NAME', 'wikibox_zyh');
+}
 
+require("modules/ModuleCaller.class.php");
+ModuleCaller::instance()->runModule('frame');
 
 ?>
---
+++ modules/frame/layout.page.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+<div>tree</div>
+<div>top: path + login/username/logout/setting</div>
+<div>main</div>
+</body>
+</html>
